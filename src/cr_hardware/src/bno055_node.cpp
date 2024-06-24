fccc9da639631a5d843316adf5a785e3acdb2476
RiverMatsumoto <matsumotorjames@gmail.com>
2024-06-24 12:30:18 -1000

    bno055 data publishing

--- src/cr_hardware/src/bno055_node.cpp
+++ src/cr_hardware/src/bno055_node.cpp
@@ -3,6 +3,8 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 #include "bno055_driver.h"
 
 class BNO055Node : public rclcpp::Node
@@ -31,7 +33,9 @@ public:
         this->declare_parameter<bool>("debug", false);
         this->declare_parameter("device", "/dev/i2c-7");
         this->declare_parameter("address", 0x28);
+        this->declare_parameter("rate_hz", 100);
 
+        // store params
         this->get_parameter("accel_offset.x", accel_offset.x);
         this->get_parameter("accel_offset.y", accel_offset.y);
         this->get_parameter("accel_offset.z", accel_offset.z);
@@ -46,6 +50,7 @@ public:
         this->get_parameter("debug", debug_);
         this->get_parameter("device", device_);
         this->get_parameter("address", address_);
+        this->get_parameter("rate_hz", rate_hz_);
 
         RCLCPP_INFO_STREAM(get_logger(), "Device: " << device_ << " Address: " << address_);
 
@@ -71,6 +76,9 @@ public:
 
         RCLCPP_INFO(get_logger(), "Started 1hz imu readout");
         timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&BNO055Node::ReadOrientation, this));
+
+        imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", 20);
+        abs_orientation_pub_ = this->create_publisher<geometry_msgs::msg::Vector3>("absolute_orientation", 20);
     }
 
     ~BNO055Node()
@@ -98,25 +106,56 @@ private:
 
     void ReadOrientation()
     {
-        bno055_accel_t accel = {0,0,0};
-        if (bno055_read_accel_xyz(&accel) != BNO055_SUCCESS)
-        {
-            RCLCPP_INFO(get_logger(), "Error reading from imu");
-        }
-        RCLCPP_INFO(get_logger(), "Accel (cm/s): X: %d, Y: %d, Z: %d", accel.x, accel.y, accel.z);
+        bno055_linear_accel_float_t lin_accel = {0,0,0};
+        bno055_gyro_float_t gyro = {0,0,0};
+        bno055_quaternion_double_t quaternion = {0,0,0,0};
+        // absolute orientation, heading pitch roll
+        bno055_euler_float_t euler = {0,0,0};
 
         // +0 to +360 degrees for heading (0 should be north)
         // -180 to +180 degrees for pitch
         // -90 to +90 degrees for roll
-        bno055_euler_float_t euler;
+        if (bno055_convert_float_linear_accel_xyz_msq(&lin_accel) != BNO055_SUCCESS)
+            RCLCPP_ERROR(get_logger(), "Error reading linear acceleration data from imu");
+        if (bno055_convert_float_gyro_xyz_rps(&gyro) != BNO055_SUCCESS)
+            RCLCPP_ERROR(get_logger(), "Error reading gyro data from imu");
+        bno055_convert_double_quaternion_wxyz(&quaternion); // no error code for some reason
         if (bno055_convert_float_euler_hpr_deg(&euler) != BNO055_SUCCESS)
-        {
-            RCLCPP_INFO(get_logger(), "Error reading from imu");
-        }
+            RCLCPP_ERROR(get_logger(), "Error reading euler absolute orientation from imu");
+        
         // adjust for 0 = north
         int mag_offset_r;
         this->get_parameter("mag_offset.r", mag_offset_r);
         euler.h = euler.h - (mag_offset_r / 16.0f);
+
+        // publish imu data
+        sensor_msgs::msg::Imu imu_msg;
+        geometry_msgs::msg::Vector3 abs_orientation_msg;
+        imu_msg.header.frame_id = "bno055_imu_link";
+        imu_msg.header.stamp = now();
+        imu_msg.linear_acceleration.x = lin_accel.x;
+        imu_msg.linear_acceleration.y = lin_accel.y;
+        imu_msg.linear_acceleration.z = lin_accel.z;
+        imu_msg.angular_velocity.x = gyro.x;
+        imu_msg.angular_velocity.y = gyro.y;
+        imu_msg.angular_velocity.z = gyro.z;
+        imu_msg.orientation.x = quaternion.x;
+        imu_msg.orientation.y = quaternion.y;
+        imu_msg.orientation.z = quaternion.z;
+        imu_msg.orientation.w = quaternion.w;
+        abs_orientation_msg.x = euler.h;
+        abs_orientation_msg.y = euler.p;
+        abs_orientation_msg.z = euler.r;
+
+        imu_pub_->publish(imu_msg);
+
+        if (debug_)
+        {
+            RCLCPP_INFO(get_logger(), "Linear accel (m/s): X: %f, Y: %f, Z: %f", lin_accel.x, lin_accel.y, lin_accel.z);
+            RCLCPP_INFO(get_logger(), "Gyro (rad/s): X: %f, Y: %f, Z: %f", gyro.x, gyro.y, gyro.z);
+            RCLCPP_INFO(get_logger(), "Quaternion: X: %f, Y: %f, Z: %f", gyro.x, gyro.y, gyro.z);
+            RCLCPP_INFO(get_logger(), "Euler orientation: Heading: %f, Pitch: %f, Roll: %f", euler.h, euler.p, euler.r);
+        }
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
@@ -124,6 +163,12 @@ private:
     int address_;
     bno055_t bno055;
     bool debug_;
+    int rate_hz_;
+
+    // publishers
+    // linear acceleration,
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr abs_orientation_pub_;
 };
 
 int main(int argc, char *argv[])
