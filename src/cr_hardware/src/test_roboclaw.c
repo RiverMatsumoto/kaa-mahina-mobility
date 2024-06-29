8f9528ed22c79ae5562a192adc2d76e87f982dee
RiverMatsumoto <matsumotorjames@gmail.com>
2024-06-28 15:32:18 -1000

    test_roboclaw fixes

--- src/cr_hardware/src/test_roboclaw.c
+++ src/cr_hardware/src/test_roboclaw.c
@@ -42,87 +42,113 @@ void informative_terminate(struct roboclaw *rc);
 int main(int argc, char **argv)
 {
 
-  struct roboclaw *rc;
-  uint8_t address = 0x80; // address of roboclaw unit
-  int16_t voltage;
-  float voltage_float;
-  int baudrate, duty_cycle;
+    struct roboclaw *rc;
+    uint8_t address = 128; // address of roboclaw unit
+    int baudrate = 115200;
+    int16_t voltage;
+    float voltage_float;
+    int duty_cycle;
+    char *device = "/dev/ttyACM0";
 
-  if (argc != 4)
-  {
-    usage(argv);
+    // device
+    if (argc == 1)
+    {
+        usage(argv);
+        printf("Using default device: /dev/ttyACM0\n");
+        printf("Using default baud rate: %d\n", baudrate);
+        printf("Using default address: %d\n", address);
+    }
+    // don't change default device
+    if (argc == 2)
+    {
+        printf("Using default baud rate: %d\n", baudrate);
+        printf("Using default address: %d\n", address);
+    }
+    // baud rate
+    if (argc == 3)
+    {
+        baudrate = (int)strtol(argv[2], NULL, 10);
+        printf("Using default address: %d\n", address);
+    }
+    // address
+    if (argc == 4)
+    {
+        baudrate = (int)strtol(argv[2], NULL, 10);
+        address = (uint8_t)strtol(argv[3], NULL, 0);
+    }
+    if (argc > 4)
+    {
+        usage(argv);
+        exit(0);
+    }
+
+    // initialize at supplied terminal at specified baudrate
+    rc = roboclaw_init(argv[1], baudrate);
+
+    if (rc == NULL)
+    {
+        perror("unable to initialize roboclaw");
+        exit(1);
+    }
+
+    printf("initialized communication with roboclaw\n");
+
+    // read the battery voltage
+    if (roboclaw_main_battery_voltage(rc, address, &voltage) != ROBOCLAW_OK)
+        informative_terminate(rc);
+
+    voltage_float = (float)voltage / 10.0f;
+    printf("battery voltage is : %f V\n", voltage_float);
+
+    printf("Getting polling rate...\n");
+    // Get polling rate
+    int count = 0;
+    time_t start, end;
+    float seconds = 3.0f;
+    start = time(NULL);
+    do
+    {
+        int res = roboclaw_main_battery_voltage(rc, address, &voltage);
+        count++;
+        end = time(NULL);
+    } while (difftime(end, start) < seconds);
+    double frequency = count / seconds;
+    printf("Total function calls: %d\n", count);
+    printf("Function calls per second (Hz): %f\n", frequency);
+    // function calls per second * 6 bytes per function call * 8 bits per byte
+    printf("Approximately %f bits per second\n", frequency * 8 * 6);
+
+    // make sure the motors are stopped before leaving
+    printf("stopping the motors..\n");
+    roboclaw_duty_m1m2(rc, address, 0, 0);
+
+    if (roboclaw_close(rc) != ROBOCLAW_OK)
+        perror("unable to shutdown roboclaw cleanly");
+
+    printf("bye...\n");
+
     return 0;
-  }
-
-  baudrate = (int)strtol(argv[2], NULL, 10);
-  address = (uint8_t)strtol(argv[3], NULL, 0);
-
-  // initialize at supplied terminal at specified baudrate
-  rc = roboclaw_init(argv[1], baudrate);
-
-  if (rc == NULL)
-  {
-    perror("unable to initialize roboclaw");
-    exit(1);
-  }
-
-  printf("initialized communication with roboclaw\n");
-
-  // read the battery voltage
-  if (roboclaw_main_battery_voltage(rc, address, &voltage) != ROBOCLAW_OK)
-    informative_terminate(rc);
-
-  voltage_float = (float)voltage / 10.0f;
-  printf("battery voltage is : %f V\n", voltage_float);
-
-  printf("WARNING - make sure it is safe for the motors to be moved\n\n");
-
-  // Get polling rate
-  int count = 0;
-  time_t start, end;
-  float seconds = 3.0f;
-  start = time(NULL);
-  do
-  {
-    int res = roboclaw_main_battery_voltage(rc, address, &voltage);
-    count++;
-    end = time(NULL);
-  } while (difftime(end, start) < seconds);
-  double frequency = count / seconds;
-  printf("Total function calls: %d\n", count);
-  printf("Function calls per second (Hz): %f\n", frequency);
-
-  // make sure the motors are stopped before leaving
-  printf("stopping the motors..\n");
-  roboclaw_duty_m1m2(rc, address, 0, 0);
-
-  if (roboclaw_close(rc) != ROBOCLAW_OK)
-    perror("unable to shutdown roboclaw cleanly");
-
-  printf("bye...\n");
-
-  return 0;
 }
 
 void usage(char **argv)
 {
-  printf("Usage:\n");
-  printf("%s terminal_device baudrate address\n\n", argv[0]);
-  printf("examples:\n");
-  printf("%s /dev/ttyACM0 38400 0x80\n", argv[0]);
-  printf("%s /dev/ttyAMA0 460800 0x81\n", argv[0]);
-  printf("%s /dev/tty_in1 115200 0x82\n", argv[0]);
+    printf("Usage:\n");
+    printf("%s terminal_device baudrate address\n\n", argv[0]);
+    printf("examples:\n");
+    printf("%s /dev/ttyACM0 38400 0x80\n", argv[0]);
+    printf("%s /dev/ttyAMA0 460800 0x81\n", argv[0]);
+    printf("%s /dev/tty_in1 115200 0x82\n", argv[0]);
 }
 
 void informative_terminate(struct roboclaw *rc)
 {
-  fprintf(stderr, "problem communicating with roboclaw\n");
-  fprintf(stderr, "make sure you are:\n");
-  fprintf(stderr, "-using correct tty device\n");
-  fprintf(stderr, "-using correct address\n");
-  fprintf(stderr, "-set correct roboclaw baudrate\n");
-  fprintf(stderr, "-wired things correctly\n");
-  fprintf(stderr, "terminating...\n");
-  roboclaw_close(rc);
-  exit(1);
+    fprintf(stderr, "problem communicating with roboclaw\n");
+    fprintf(stderr, "make sure you are:\n");
+    fprintf(stderr, "-using correct tty device\n");
+    fprintf(stderr, "-using correct address\n");
+    fprintf(stderr, "-set correct roboclaw baudrate\n");
+    fprintf(stderr, "-wired things correctly\n");
+    fprintf(stderr, "terminating...\n");
+    roboclaw_close(rc);
+    exit(1);
 }
