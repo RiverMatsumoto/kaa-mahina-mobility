096a284374c66a6452781a982cf8362930dc4fc1
RiverMatsumoto <matsumotorjames@gmail.com>
2024-08-16 12:33:45 -1000

    record trial script

--- src/cr_bag/cr_bag/record_trial.py
+++ src/cr_bag/cr_bag/record_trial.py
@@ -27,7 +27,9 @@ class BagRecorder(Node):
             "/imu",
             "/absolute_orientation",
             "/differential_drive_controller/cmd_vel",
-            "/joint_states"
+            "/joint_states",
+            "/rpi1/image_raw/compressed",
+            "/rpi2/image_raw/compressed"
         ]
 
     def start_recording_callback(self, request, response):
